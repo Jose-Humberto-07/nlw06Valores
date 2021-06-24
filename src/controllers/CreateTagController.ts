3937ebce5ae3d6deba3f95cb6330e770c8855b48
José Humberto <jhpnascimento96@gmail.com>
2021-06-24 16:26:30 -0300

    terceiro dia

---
+++ src/controllers/CreateTagController.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from "express";
+import { CreateTagService } from "../services/CreateTagService";
+
+
+
+class CreateTagController {
+    async handle(request: Request, response: Response) {
+        const { name } = request.body;
+
+        const createUserService = new CreateTagService();
+
+        const tag = await createUserService.execute(name);
+
+        return response.json(tag);
+    }
+    
+}
+
+
+export {CreateTagController};
